64e2484bd2cce706cec61b51d765e83ccdd30acd
Roman frontend <mail@wbcplace.ru>
2022-11-28 14:06:08 +0700

    Edited Modal

--- src/components/ModalDialog.vue
+++ src/components/ModalDialog.vue
@@ -1,27 +1,58 @@
 <template>
-  <div class="modal-backdrop">
-    <div class="modal" >
-      <header class="modal-header">
-        <button type="button" class="btn-close" @click="close">x</button>
+  <div v-if="isOpen" class="modal-backdrop" @click="close">
+    <div class="modal" @click.stop>
 
-        <slot> default header </slot>
+      <slot></slot>
 
-        <button type="button" class="btn-simple" @click="close">OK</button>
-        <button type="button" class="btn-simple" @click="close">Cancel</button>
-      </header>
+      <div class="modal-header">
+        <slot name="actions" :close="close" :confirm="confirm">
+          <button  @click="confirm">Ok</button>
+        </slot>
+
+        <slot name="reject" :close="close">
+          <button type="button"  @click="close">Отмена</button>
+        </slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+
+  //initial reactive state
+  data: () => ({
+    isOpen: false,
+    currentModalController: null,
+    modalController:null,
+  }),
+
   methods: {
-    //emitting event to HomeView.vue
-    close() {
-      this.$emit("close");
+
+    //  show Modal
+    show() {
+      let resolve;
+      let reject;
+      const modalPromise = new Promise((ok, fail) => {
+        resolve = ok;
+        reject = fail;
+      });
+
+      this.modalController = {resolve, reject};
+      this.isOpen = true;
+      // to modalResult
+      return modalPromise;
     },
-    show() {},
+
+    confirm() {
+      this.modalController.resolve(true);
+      this.isOpen = false;
+    },
+
+    close() {
+      this.modalController.resolve(false);
+      this.isOpen = false;
+    },
   },
 };
 </script>
@@ -56,37 +87,5 @@ export default {
   position: relative;
   border-bottom: 1px solid #eeeeee;
   justify-content: space-between;
-}
-
-.btn-close {
-  position: absolute;
-  top: 0;
-  right: 0;
-  border: none;
-  font-size: 20px;
-  padding: 10px;
-  cursor: pointer;
-  font-weight: bold;
-  color: #4aae9b;
-  background: transparent;
-}
-
-.btn-green {
-  color: white;
-  background: #4aae9b;
-  border: 1px solid #4aae9b;
-  border-radius: 2px;
-}
-.btn-simple {
-  cursor: pointer;
-  background-color: #4caf50; /* Green */
-  border: none;
-  color: white;
-  padding: 5px 10px;
-  margin: 5px 0;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="home">
-    <ModalDialog v-show="isModalVisible" @close="closeModal">
-      <template> This is a new modal header</template>
+    <button @click="showModal">show modal</button>
+    <ModalDialog ref="confirmModal">
+     <p>Are you sure?</p>
+      <template #actions="{confirm}">
+        <button @click="confirm" type="button" class="btn-simple" :disabled="isConfirmationCorrect">OK</button>
+      </template>
+
+      <template #reject="{close}">
+        <button @click="close" type="button" class="btn-close" :disabled="isConfirmationReject">Cancel</button>
+      </template>
     </ModalDialog>
-    <button type="button" class="btn" @click="showModal">Show Modal</button>
   </div>
 </template>
 
@@ -15,17 +22,70 @@ export default {
   components: {
     ModalDialog,
   },
+
+
   data: () => ({
-    isModalVisible: false,
+    confirmation: null,
+    CONFIRMATION_TEXT: "OK",
+    REJECT_TEXT: "CANCEL",
   }),
 
-  methods: {
-    showModal() {
-      this.isModalVisible = true;
+
+
+  //buttons validations
+  computed: {
+    isConfirmationCorrect() {
+      return this.confirmation === this.CONFIRMATION_TEXT;
     },
-    closeModal() {
-      this.isModalVisible = false;
+    isConfirmationReject() {
+      return this.confirmation === this.REJECT_TEXT;
+    },
+  },
+
+
+  methods: {
+    async showModal() {
+      //ModalDialog component opens itself on click
+      const modalResult = await this.$refs.confirmModal.show();
+
+      if (modalResult) {
+        alert("Confirmed!");
+      } else {
+        alert("Declined!")
+      }
     },
   },
 };
 </script>
+<style>
+.home {
+  margin-top: 2rem;
+}
+
+.btn-close {
+  cursor: pointer;
+  background-color: #af4f4c; /* Red */
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  margin: 5px 0;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+
+.btn-simple {
+  cursor: pointer;
+  background-color: #4caf50; /* Green */
+  border: none;
+  color: white;
+  padding: 5px 10px;
+  margin: 5px 0;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
